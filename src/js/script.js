eebbe98ea4114f5a230e273c48c9c1272eea212d
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-03-29 20:49:20 +0200

    Add splash

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,27 @@ const app = {
 };
 
 app.init();
+
+const navbarSticky = function() {
+
+  /* SOURCE */
+  /* https://webgolovolomki.com/en/how-to-add-a-css-class-on-scroll/ */
+
+  let scrollpos = window.scrollY;
+
+  const header = document.querySelector('.splash');
+  const scrollChange = 100;
+
+  const add_class_on_scroll = () => header.classList.add('scroll');
+  const remove_class_on_scroll = () => header.classList.remove('scroll');
+
+  window.addEventListener('scroll', function() { 
+    scrollpos = window.scrollY;
+
+    if (scrollpos >= scrollChange) { add_class_on_scroll(); }
+    else { remove_class_on_scroll(); }
+    
+  });
+};
+
+navbarSticky();
